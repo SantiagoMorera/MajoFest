d5d00fd003adefb057aa82146aaf2197e80a56a7
Santiago Morera <santi.iztli@gmail.com>
2023-05-24 22:25:11 -0600

    Final update

--- js/alerta.js
+++ js/alerta.js
@@ -1,21 +1,23 @@
 
-function alerta() {
+const alerta = () => {
   Swal.fire({
-    imageUrl: 'https://media1.giphy.com/media/UVMaSzWhq7NnPulCOd/giphy.gif?cid=ecf05e479fhtxv7qp8lpdt95ndo6xi5nibtayrh9v54c7p7z&ep=v1_gifs_related&rid=giphy.gif&ct=s',
+    imageUrl: '/img/trolls/output-onlinegiftools.gif',
     imageWidth: 200,
     imageHeight: 250,
-    text: 'María José',
-    title: '<span>¡</span>Ven a celebrar el cumpleaños de<span>:</span>',
-    confirmButtonText: 'Abrir',
+    html:
+      '<div class="alet-text-container" > <div class="ven-a-celebrar"   > <span>¡</span>Ven a celebrar el cumpleaños de<span>:</div></span> ' +
+      ' <div class="maria-jose" >María José<span>!</span></div> ' +
+      '</div>',
+    confirmButtonText: 'abrir!',
     confirmButtonColor: '#9c0dcb',
     color: '#000',
     background: '#ff81d8d4',
     backdrop: 'rgba(0,0,0,0.1)',
     showClass: {
-      popup: 'animate__animated animate__backInDown'
+      popup: 'animate__animated animate__jackInTheBox'
     },
     hideClass: {
-      popup: 'animate__animated animate__bounceOut'
+      popup: 'animate__animated animate__hinge'
     },
 
 
@@ -23,26 +25,3 @@ function alerta() {
 }
 alerta();
 
-
-/* function alertaDiaDeLaBoda() {
-  Swal.fire({
-    imageUrl: 'https://media0.giphy.com/media/keURpmUeGAjvGZ2VTJ/giphy.gif?cid=ecf05e47msqc9smayhyco5ux0ilwcqccumjx6c2ctywf3006&rid=giphy.gif&ct=s',
-    imageWidth: 150,
-    imageHeight: 150,
-    confirmButtonText: 'Abrir',
-    color: '#000',
-    background: '#ffffffb3',
-    backdrop: 'rgba(0,0,0,0.7)',
-    showClass: {
-      popup: 'animate__animated animate__backInDown'
-    },
-    hideClass: {
-      popup: 'animate__animated animate__bounceOut'
-    },
-    footer: 'Invitacion para 2 personas',
-    html: '<div class="html-alert" > Hoy es el gran dia! <br>Gracias por acompañarnos. </div>'
-
-
-  });
-}
-alertaDiaDeLaBoda(); */
